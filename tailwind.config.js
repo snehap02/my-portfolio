978c777e6b6bcdc4b190f30a90cc6c11dd300c39
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-01-25 18:42:02 +0530

    added animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
       colors:{
         smallhead: '#b8c1ec',
         type: '#eebbc3'
+      },
+      animation:{
+        anim: 'anim 4s steps(12) infinite',
+      },
+      keyframes:{
+        anim:{
+          '40%, 60%': {left:'100%'},
+          '100%': {left: '0%'}
+        }
       }
     },
   },
