f92332bdd2709a6f26721ee0c7d0ca68292bb00b
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-01-31 15:26:39 +0530

    navbar changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,13 @@ module.exports = {
       colors:{
         smallhead: '#b8c1ec',
         type: '#eebbc3',
-        bg: '#d4d8f0'
+        bg: '#d4d8f0',
+        yellow: '#fdc21d'
       },
       backgroundImage:{
-        back: 'linear-gradient(90deg, rgba(255,205,0,1) 50%, rgba(0,0,0,1) 50%)'
+        back: 'linear-gradient(90deg, rgba(255,205,0,1) 50%, rgba(0,0,0,1) 50%)',
+        back2: 'linear-gradient(90deg, rgba(0,0,0,1) 56%, rgba(225,205,0,1) 56%)',
+        back3: 'linear-gradient(315deg, rgba(225,205,0,1) 56%, rgba(0,0,0,1) 56%)'
       },
       screens:{
         'before-sm': '500px',
