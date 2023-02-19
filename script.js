5dc66f836461ece4284d6694231059d5ac78f2a8
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-02-19 15:54:44 +0530

    small animation added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 AOS.init({
     duration: 1000
 });
+
+const inputName = document.getElementById('namebox');
+const box = document.querySelectorAll('input');
+
+inputName.addEventListener('click', () => {
+    for(let singlebox of box){
+        singlebox.classList.add('border-l-4', 'border-r-4', 'border-yellow', 'transition-all');
+    }
+})
