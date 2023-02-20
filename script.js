1e7f75a6331a2687c6bf49c86928603e7d1da571
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-02-20 20:42:04 +0530

    icon added and some properties changed in html

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ AOS.init({
 
 const inputName = document.getElementById('namebox');
 const box = document.querySelectorAll('input');
+const msg = document.getElementById('message');
 
 inputName.addEventListener('click', () => {
     for(let singlebox of box){
         singlebox.classList.add('border-l-4', 'border-r-4', 'border-yellow', 'transition-all');
     }
+    msg.classList.add('border-l-4', 'border-r-4', 'border-yellow', 'transition-all');
 })
